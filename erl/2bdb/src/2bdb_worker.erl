5675f105e0d00028693620f1e3b2145a982f5b34
k32 <10274441+k32@users.noreply.github.com>
2019-06-19 06:08:47 +0200

    Prototyped transaction importing

--- erl/2bdb/src/2bdb_worker.erl
+++ erl/2bdb/src/2bdb_worker.erl
@@ -1,6 +1,8 @@
--module('2bdb_consumer').
+-module('2bdb_worker').
 
 -behaviour(gen_statem).
+
+-include_lib("stdlib/include/assert.hrl").
 
 %% API
 -export([start_link/0]).
@@ -66,25 +68,25 @@
                | in_sync
                .
 
+-type os_timestamp() :: integer().
+
 -record(try_write_trans,
-        { txid   :: txid()
-        , reads  :: [key_hash()]
-        , writes :: [{_Key, _Value}]
+        { txid     :: txid()
+        , ts_begin :: os_timestamp() %% For performance measurements only
+        , reads    :: [key_hash()]
+        , writes   :: [{_Key, _Value}]
         }).
 
--type producer() :: pid().
-
--type consumer() :: pid().
-
 -record(data,
-        { window_size     = 1 :: non_neg_integer()
-        , lock_expiration = 0 :: non_neg_integer()
-        , num_imported    = 0 :: non_neg_integer()
-        , offset          = 0 :: offset()
-        , seqno_tab           :: ets:tid()
-        , callback_tab        :: ets:tid()
-        , producer_ref        :: producer()
-        , consumer_ref        :: consumer()
+        { storage = ets_wrapper :: module()
+        , window_size     = 1   :: non_neg_integer()
+        , lock_expiration = 0   :: non_neg_integer()
+        , num_replayed    = 0   :: non_neg_integer()
+        , offset          = 0   :: offset()
+        , seqno_tab             :: ets:tid()
+        , callback_tab          :: ets:tid()
+        , producer_data         :: producer()
+        , consumer_data         :: consumer()
         }).
 
 -record(seqno_cache,
@@ -98,6 +100,7 @@
         , reply_to         :: gen_statem:from()
         , estimated_offset :: offset() %% This offset should not be very precise,
                                        %% it's used only for hangup detection
+        , ts_begin         :: os_timestamp()
         }).
 
 %%%===================================================================
@@ -117,10 +120,9 @@ start_link() ->
     gen_statem:start_link({local, ?SERVER}, ?MODULE, [], []).
 
 
--spec try_write_trans(boolean(), [key_hash()], [{_Key, _Value}]) ->
-                             ok | retry.
-try_write_trans(GrabLock, Reads, WriteOps) ->
-    gen_statem:call(?SERVER, {try_write_trans, GrabLock, Reads, Writes}).
+-spec try_write_trans(boolean(), #try_write_trans{}) -> ok | retry.
+try_write_trans(GrabLock, Tx) ->
+    gen_statem:call(?SERVER, {try_write_trans, Tx}).
 
 %%%===================================================================
 %%% gen_statem callbacks
@@ -138,12 +140,16 @@ callback_mode() -> handle_event_function.
                   gen_statem:init_result(term()).
 init([]) ->
     process_flag(trap_exit, true),
-    TabOpts     = [],
-    SeqNoTab    = ets:new('2bdb_seqno_tab', TabOpts),
-    CallbackTab = ets:new( '2bdb_callback_tab'
-                         , TabOpts ++ [{keypos, #callback_ref.txid}]
+    SeqNoTab    = ets:new( '2bdb_seqno_tab'
+                         , [ {keypos, #seqno_cache.key}
+                           , protected
+                           , named_table
+                           ]
                          ),
-    InitialData = #data{ seqno_tab = SeqNoTab
+    CallbackTab = ets:new( '2bdb_callback_tab'
+                         , [{keypos, #callback_ref.txid}]
+                         ),
+    InitialData = #data{ seqno_tab    = SeqNoTab
                        , callback_tab = CallbackTab
                        },
     %% TODO: read persisted data
@@ -157,8 +163,12 @@ init([]) ->
 %% @end
 %%--------------------------------------------------------------------
 %% Handle import transaction:
-handle_event({call, From}, {import_trans, Offset, Tx = #tx{}}, OldState, Data0) ->
-    NextState = if Data#data.num_imported >= WindowSize,
+handle_event({call, From}, {import_trans, TrueOffset, Tx}, OldState, Data0) ->
+    Data = handle_import_trans(Data0, TrueOffset, Tx),
+    #data{ num_replayed = NumImported
+         , window_size  = WindowSize
+         } = Data,
+    NextState = if NumImported >= WindowSize,
                    OldState =:= incomplete_window ->
                         in_sync;
                    true ->
@@ -168,7 +178,7 @@ handle_event({call, From}, {import_trans, Offset, Tx = #tx{}}, OldState, Data0) 
 %% Handle local transactions:
 handle_event({call, From}, Tx = #try_write_trans{}, in_sync, Data) ->
     %% We are ready to write transactions:
-    do_trans(Data, From, Tx);
+    do_write_trans(Data, From, Tx);
 handle_event({call, _}, #try_write_trans{}, hold_on, _Data) ->
     %% We need to back off. Queue up this transaction for now
     {keep_state_and_data, [postpone]};
@@ -226,27 +236,25 @@ code_change(_OldVsn, State, Data, _Extra) ->
 %%% Internal functions
 %%%===================================================================
 
--spec do_trans( #data{}
-              , gen_statem:from()
-              , #try_write_trans{}
-              ) -> keep_state_and_data.
-do_trans(Data, From, Tx) ->
+-spec do_write_trans( #data{}
+                    , gen_statem:from()
+                    , #try_write_trans{}
+                    ) -> keep_state_and_data.
+do_write_trans(Data, From, Tx) ->
     #data{ seqno_tab    = SeqNoTab
          , callback_tab = CallbackTab
          , offset       = Offset
          } = Data
-    #try_write_trans{ txid   = Txid
-                    , reads  = Reads
-                    , writes = Writes
+    #try_write_trans{ txid     = Txid
+                    , ts_begin = TsBegin
+                    , reads    = Reads
+                    , writes   = Writes
                     } = Tx,
     ReadOps = [{KeyHash, get_seqno(Data, KeyHash)} || KeyHash <- Reads],
     WriteOps = [{Key, get_seqno(Data, key_hash(Key)), Val}
                 || {Key, Val} <- Writes],
     %% Produce transaction to the tlog (asynchronously, because we are
     %% very optimistic!):
-    %%
-    %% Note: brod retransmissions are fine, as long as they happen
-    %% within window. TODO: investigate that
     produce(#tx{ id         = Txid
                , partitions = []   %% TODO: Chop it up
                , reads      = ReadOps
@@ -257,10 +265,39 @@ do_trans(Data, From, Tx) ->
               , #callback_ref{ txid             = Txid
                              , reply_to         = From
                              , estimated_offset = Offset
+                             , ts_begin         = TsBegin
                              }
               ),
     %% Don't reply yet:
     keep_state_and_data.
+
+-spec handle_import_trans(#data{}, offset(), #tx{}) -> #data{}.
+handle_import_trans(Data0, TrueOffset, Tx) ->
+    #data{ window_size  = WindowSize
+         , num_replayed = NumReplayed
+         } = Data0,
+    #tx{ id               = TxId
+       , estimated_offset = EstimatedOffset
+       , writes           = WriteOps
+       , reads            = ReadOps
+       } = Tx,
+    ?assert(EstimatedOffset =< TrueOffset),
+    WriteSeqNos = [{key_hash(Key), SeqNo}
+                   || {Key, SeqNo, _} <- WriteOps],
+    Valid = (TrueOffset - EstimatedOffset < WindowSize) andalso
+            validate_locks(Data0, WriteOps) andalso
+            validate_seqnos(Data0, WriteSeqNos) andalso
+            validate_seqnos(Data0, ReadOps),
+    Data = Data0#data{ offset       = TrueOffset
+                     , num_replayed = NumReplayed + 1
+                     },
+    if Valid ->
+            do_import_transaction(WriteOps),
+            maybe_complete_transaction(Data, ok);
+       true ->
+            maybe_complete_transaction(Data, retry)
+    end,
+    Data.
 
 -spec get_seqno(#data{}, key_hash()) -> seqno().
 get_seqno(Data, KeyHash) ->
@@ -275,3 +312,58 @@ get_seqno(Data, KeyHash) ->
         _ ->
             0
     end.
+
+-spec do_import_transaction(#data{}, write_ops()) -> ok.
+do_import_transaction(Data, WriteOps) ->
+    Storage:put([{Key, Val} || {Key, _, Val} <- WriteOps]),
+    bump_seqnos(Data, WriteOps).
+
+-spec bump_seqnos(#data{}, write_ops()) -> ok.
+bump_seqnos(Data, WriteOps) ->
+    #data{ seqno_tab = SeqNoTab
+         , offset    = Offset
+         , storage   = Storage
+         } = Data,
+    Inserts = [#seqno_cache{ key              = key_hash(Key)
+                           , seqno            = OldSeqNo + 1
+                           , last_seen_offset = Offset
+                           }
+               || {Key, OldSeqNo, _} <- WriteOps],
+    ets:insert(SeqNoTab, Inserts),
+    ok.
+
+-spec validate_locks(#data{}, write_ops()) -> boolean().
+validate_locks(Data, WriteOps) ->
+    %% TODO: Not implemented
+    true.
+
+-spec validate_seqnos(#data{}, [{key_hash(), seqno()}]) -> boolean().
+validate_seqnos(Data, SeqNos) ->
+    try
+        [case get_seqno(Data, KeyHash) of
+             ExpectedSeqNo -> ok;
+             TrueSeqNo     -> throw(mismatch)
+         end
+         || {KH, ExpectedSeqNo} <- SeqNos],
+        true
+    catch
+        mismatch -> false
+    end.
+
+%% Signal `Result' to the transaction process
+-spec maybe_complete_transaction(#data{}, txid(), _Result) -> ok.
+maybe_complete_transaction(Data, TxId, Result) ->
+    #data{callback_tab = Tab} = Data,
+    case ets:take(Tab, TxId) of
+        [#callback_ref{reply_to = From}] ->
+            gen_statem:reply(From, Result);
+        [] ->
+            %% This transaction didn't originate from our server,
+            %% ignore it.
+            ok
+    end.
+
+-spec produce(#data{}, #tx{}) -> ok.
+produce(#data{producer_data = ProducerData}, Tx) ->
+    %% TODO: Not implemented
+    ok.
