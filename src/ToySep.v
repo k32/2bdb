1cd5946220bd67d42eb32bf601ac8a65b1ab1bb0
k32 <10274441+k32@users.noreply.github.com>
2020-01-24 23:53:22 +0100

    Mockup of trace-based forward reasoning

--- src/ToySep.v
+++ src/ToySep.v
@@ -7,22 +7,26 @@ From Coq Require Import
 From Containers Require Import
      OrderedType
      OrderedTypeEx
-     Maps.
+     MapInterface
+     MapFacts
+     MapAVLInstance.
 
 From QuickChick Require Import
      QuickChick.
 
 Import ListNotations.
 
+Reserved Notation "aid '@' req '<~' ret" (at level 30).
+
 Section IOHandler.
   Variable AID : Set.
   Context {AID_ord : OrderedType AID}.
 
-  Inductive HandlerRet {req_t state_t : Type} (req : req_t) {rep_t : req_t -> Set} : Type :=
+  Inductive HandlerRet {req_t state_t : Type} (req : req_t) (rep_t : req_t -> Set) : Type :=
   | r_return : forall (reply : rep_t req)
                  (state' : state_t)
-    , HandlerRet req
-  | r_stall : HandlerRet req.
+    , HandlerRet req rep_t
+  | r_stall : HandlerRet req rep_t.
 
   Notation "'Nondeterministic' a" := ((a) -> Prop) (at level 200).
 
@@ -39,18 +43,17 @@ Section IOHandler.
     }.
 
   CoInductive Actor {T : t} : Type :=
-  | a_sync1 : forall (pending_req : T.(h_req))
-                (continuation : T.(h_ret) pending_req -> Actor)
-              , Actor
-  | a_sync2 : forall (req : T.(h_req))
-                (pending_rep : T.(h_ret) req)
-                (continuation : T.(h_ret) req -> Actor)
-              , Actor
+  | a_sync : forall (pending_req : T.(h_req))
+               (continuation : T.(h_ret) pending_req -> Actor)
+             , Actor
   | a_dead : Actor.
 
   Definition Actors {H : t} := Map[AID, (@Actor H)].
 
-  Definition Trace {H : t} := list H.(h_req).
+  Inductive TraceElem {H : t} :=
+  | trace_elem : forall (aid : AID) (req : H.(h_req)) (ret : H.(h_ret) req), TraceElem.
+
+  Definition Trace {H : t} := list (@TraceElem H).
 
   Record ModelState {H : t} : Type :=
     mkState
@@ -60,101 +63,110 @@ Section IOHandler.
         m_trace  : @Trace H;
       }.
 
-  (* TODO: Update trace! *)
+  Definition enter_syscall {H} aid (req : h_req H) cont (ret : HandlerRet req (h_ret H)) ms :=
+    match ms with
+      {| m_state := s; m_actors := aa; m_trace := t |} =>
+      match ret with
+      | r_stall _ _         => ms
+      | r_return _ _ rep s' =>
+        let a' := cont rep in
+        let aa' := add aid a' aa in
+        mkState H aa' s' (trace_elem aid req rep :: t)
+      end
+    end.
+
+  (* TODO: Reap zombie actors *)
   Definition eval_sync {H : t} (aid : AID) (ms : ModelState) (ms' : ModelState) : Prop :=
     match ms with
       {| m_state := s; m_actors := aa; m_trace := t |} =>
       match find aid aa with
       | None =>
         ms = ms'
-
       | Some a_dead =>
         ms = ms'
-
-      | Some (a_sync1 req cont) =>
+      | Some (a_sync req cont) =>
         forall ret : @HandlerRet H.(h_req) H.(h_state) req H.(h_ret),
           H.(h_eval) s aid req ret ->
-          match ret with
-          | r_stall _         => ms = ms'
-          | r_return _ rep s' => let a' := a_sync2 req rep cont in
-                                let aa' := add aid a' aa in
-                                ms' = mkState H aa' s' t
-          end
-
-      | Some (a_sync2 req ret cont) =>
-          let a' := cont ret in
-          ms' = mkState H (add aid a' aa) s t
+          ms' = enter_syscall aid req cont ret ms
       end
     end.
 
-  (** Define all schedulings of the system *)
-  Inductive Scheduling {H : t} initial_actors : ModelState -> Prop :=
+  (* Ltac forward1 ms aid ret := *)
+
+  (** Constructively define a property that certain model state is
+  reachable from a given deterministic program [initial_actors] and
+  nondeterministic handler state *)
+  Inductive ReachableState {H : t} {initial_actors : Actors} : ModelState -> Prop :=
   | model_init : forall s0,
       H.(h_initial_state) s0 ->
-      Scheduling initial_actors {| m_actors := initial_actors;
-                                   m_state  := s0;
-                                   m_trace  := nil;
-                                |}
+      ReachableState {| m_actors := initial_actors;
+                        m_state  := s0;
+                        m_trace  := nil;
+                     |}
 
   | model_step : forall (aid : AID) (ms : ModelState),
       forall ms', eval_sync aid ms ms' ->
-             Scheduling initial_actors ms ->
-             Scheduling initial_actors ms'.
+             ReachableState ms ->
+             ReachableState ms'.
 
   Definition exit {T : t} : @Actor T := a_dead.
 
-  Definition compose_state h1 h2 : Type :=
-    h_state h1 * h_state h2.
+  Section ComposeHandlers.
+    Definition compose_state h1 h2 : Type :=
+      h_state h1 * h_state h2.
 
-  Definition compose_req h1 h2 : Set :=
-    h_req h1 + h_req h2.
+    Definition compose_req h1 h2 : Set :=
+      h_req h1 + h_req h2.
 
-  Hint Transparent compose_state.
+    Hint Transparent compose_state.
 
-  Definition compose_initial_state h1 h2 state :=
-    h1.(h_initial_state) (fst state) /\ h2.(h_initial_state) (snd state).
+    Definition compose_initial_state h1 h2 state :=
+      h1.(h_initial_state) (fst state) /\ h2.(h_initial_state) (snd state).
 
-  Hint Transparent compose_initial_state.
+    Hint Transparent compose_initial_state.
 
-  Definition compose_ret_t (h1 h2 : t) (req : compose_req h1 h2) : Set :=
-    match req with
-    | inl l => h1.(h_ret) l
-    | inr r => h2.(h_ret) r
-    end.
+    Definition compose_ret_t (h1 h2 : t) (req : compose_req h1 h2) : Set :=
+      match req with
+      | inl l => h1.(h_ret) l
+      | inr r => h2.(h_ret) r
+      end.
 
-  Hint Transparent compose_ret_t.
+    Hint Transparent compose_ret_t.
 
-  Definition ComposeHandlerRet h1 h2 req : Type :=
-    @HandlerRet (compose_req h1 h2)
-                (compose_state h1 h2)
-                req
-                (compose_ret_t h1 h2).
+    Definition ComposeHandlerRet h1 h2 req : Type :=
+      @HandlerRet (compose_req h1 h2)
+                  (compose_state h1 h2)
+                  req
+                  (compose_ret_t h1 h2).
 
-  Definition compose_eval h1 h2
-             (state : compose_state h1 h2)
-             (aid : AID)
-             req
-             (ret : ComposeHandlerRet h1 h2 req) : Prop.
-    refine (
-        (let (s1, s2) := state in
-         match req as req' return (req = req' -> Prop) with
-         | inl req' => fun H => h1.(h_eval) s1 aid req' _
-         | inr req' => fun H => h2.(h_eval) s2 aid req' _
-         end) (eq_refl req));
-      subst;
-      destruct ret;
-      try simpl in reply;
-      constructor; assumption.
-  Defined.
+    Definition compose_eval h1 h2
+               (state : compose_state h1 h2)
+               (aid : AID)
+               req
+               (ret : ComposeHandlerRet h1 h2 req) : Prop.
+      refine (
+          (let (s1, s2) := state in
+           match req as req' return (req = req' -> Prop) with
+           | inl req' => fun H => h1.(h_eval) s1 aid req' _
+           | inr req' => fun H => h2.(h_eval) s2 aid req' _
+           end) (eq_refl req));
+        subst;
+        destruct ret;
+        try simpl in reply;
+        constructor; assumption.
+    Defined.
 
-  Definition compose (h1 h2 : t) : t :=
-    {| h_state         := compose_state h1 h2;
-       h_req           := compose_req h1 h2;
-       h_ret           := compose_ret_t h1 h2;
-       h_initial_state := compose_initial_state h1 h2;
-       h_eval          := compose_eval h1 h2;
-    |}.
+    Definition compose (h1 h2 : t) : t :=
+      {| h_state         := compose_state h1 h2;
+         h_req           := compose_req h1 h2;
+         h_ret           := compose_ret_t h1 h2;
+         h_initial_state := compose_initial_state h1 h2;
+         h_eval          := compose_eval h1 h2;
+      |}.
+  End ComposeHandlers.
 End IOHandler.
+
+Notation "aid '@' req '<~' ret" := (trace_elem aid req ret).
 
 Arguments compose {_}.
 Arguments h_state {_}.
@@ -163,15 +175,14 @@ Arguments h_ret {_}.
 Arguments h_initial_state {_}.
 Arguments h_eval {_}.
 Arguments Actor {_} {_}.
-Arguments a_sync1 {_} {_}.
-Arguments a_sync2 {_} {_}.
+Arguments a_sync {_} {_}.
 Arguments a_dead {_} {_}.
 Arguments exit {_} {_}.
 Arguments ModelState {_} {_}.
 Arguments m_actors {_} {_} {_}.
 Arguments m_state {_} {_} {_}.
 Arguments m_trace {_} {_} {_}.
-Arguments Scheduling {_} {_}.
+Arguments ReachableState {_} {_}.
 Arguments model_init {_} {_} {_} {_}.
 Arguments model_step {_} {_} {_} {_}.
 
@@ -191,8 +202,8 @@ Module Mutable.
 
   Local Definition eval_f (s0 : T) (aid : AID) (req : req_t) : @HandlerRet req_t T req ret_t :=
     match req with
-    | get   => r_return _ s0 s0
-    | put x => r_return _ I x
+    | get   => r_return _ _ s0 s0
+    | put x => r_return _ _ I x
     end.
 
   Definition t : (t AID) :=
@@ -219,9 +230,9 @@ Module Mutex.
 
   Local Definition eval_f (s0 : state_t) (aid : AID) (req : req_t) : @HandlerRet req_t state_t req ret_t :=
     match req, s0 with
-    | grab, None   => r_return _ I (Some aid)
-    | grab, Some _ => r_stall _
-    | release, _   => r_return _ I None
+    | grab, None   => r_return _ _ I (Some aid)
+    | grab, Some _ => r_stall _ _
+    | release, _   => r_return _ _ I None
     end.
 
   Definition t : (t AID) :=
@@ -239,7 +250,7 @@ Module ExampleModelDefn.
 
   Local Definition handler := compose (Mutable.t AID nat (fun a => a = 0)) (Mutex.t AID).
 
-  Notation "'do' V '<-' I ; C" := (@a_sync1 AID handler (I) (fun V => C))
+  Notation "'do' V '<-' I ; C" := (@a_sync AID handler (I) (fun V => C))
                                   (at level 100, C at next level, V ident, right associativity).
 
   Local Definition put (val : nat) : handler.(h_req) :=
@@ -254,10 +265,10 @@ Module ExampleModelDefn.
   Local Definition release : h_req handler :=
     inr (Mutex.release).
 
-  (* Just a demonstration that we can define programs that loop
-  indefinitely, as long as they do IO: *)
+  (* Just a demonstration how to define a programs that loops
+  indefinitely, as long as it does IO: *)
   Local CoFixpoint infinite_loop (aid : AID) : Actor :=
-    a_sync1 (put 0) (fun ret => infinite_loop aid).
+    a_sync (put 0) (fun ret => infinite_loop aid).
 
   (* Data race example: *)
   Local Definition counter_race (_ : AID) : Actor :=
@@ -272,14 +283,33 @@ Module ExampleModelDefn.
     do _ <- put (v + 1);
     do _ <- release; exit.
 
-  Local Definition two_actors := add 0 (counter_race 0) (add 1 (counter_race 1) [] : Map [nat, Actor]).
+  Local Definition two_actors := add 0 (counter_race 0) (add 1 (counter_race 1) (empty _) : Map [nat, Actor]).
 
-  Example race1 :=
+  Example race1 :
     exists (s : ModelState handler),
-      Scheduling handler two_actors s ->
-      find 0 (m_actors s) = Some a_dead ->
-      find 1 (m_actors s) = Some a_dead ->
+      ReachableState handler two_actors s ->
+      find 0 (m_actors s) = Some a_dead /\ find 1 (m_actors s) = Some a_dead ->
       fst (m_state s) = 1.
+  Proof.
+    set (ms0 := {| m_actors := two_actors;
+                   m_state  := (0, None);
+                   m_trace  := [];
+                |}).
+    forward ms0
+            [0 @ get <~ 0;
+             1 @ get <~ 0;
+             0 @ put 1 <~ True;
+             1 @ put 1 <~ True
+            ].
+
+
+               1:
+       (1, get, 0),
+      ]
+
+    intros s Hrs [Hf1 Hf2].
+    inversion H as [s0 Hs0|aid ms].
+    - destruct Hs0.
 
 
 End ExampleModelDefn.
