18b88150bc610d3c3dc12b243b69bd1d89329328
k32 <10274441+k32@users.noreply.github.com>
2020-03-25 22:58:22 +0100

    Remove old Erlang prototype

--- erl/2bdb/src/2bdb_int.hrl
+++
@@ -1,9 +0,0 @@
--ifndef(_2BDB_INT_HRL_).
--define(_2BDB_INT_HRL_, 1).
-
--include_lib("hut/include/hut.hrl").
--include_lib("snabbkaffe/include/snabbkaffe.hrl").
-
--define(brod_client, '2bdb_client').
-
--endif.
