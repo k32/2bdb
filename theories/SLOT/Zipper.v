1252e2f8c0dccb251d053510ac336f0f5ef72688
k32 <10274441+k32@users.noreply.github.com>
2020-12-09 00:35:41 +0100

    Zipper with deletion

--- theories/SLOT/Zipper.v
+++ theories/SLOT/Zipper.v
@@ -9,21 +9,47 @@ Import ListNotations.
 Section defn.
   Context {V : Type}.
 
-  Definition t: Type := list V * V * list V.
+  Definition t: Type := list V * option V * list V.
 
-  Definition movl (z : t) : t :=
+  Definition to_list (z : t) : list V :=
     match z with
-    | ([], e, r) => ([], e, r)
-    | (l :: rest, e, r) => (rest, l, e :: r)
+    | (l, None, r) => rev l ++ r
+    | (l, Some e, r) => rev l ++ (e :: r)
     end.
 
-  Definition movr (z : t) : t :=
+  Definition mov_l (z : t) : t :=
     match z with
-    | (l, e, []) => (l, e, [])
-    | (l, e, r :: rest) => (e :: l, r, rest)
+    | ([], Some e, r)       => ([], None, e :: r)
+    | ([], None, r)         => ([], None, r)
+    | (l :: rest, Some e, r) => (rest, Some l, e :: r)
+    | (l :: rest, None, r)   => (rest, Some l, r)
     end.
 
-  Definition get (z : t) : V :=
+  Definition mov_r (z : t) : t :=
+    match z with
+    | (l, Some e, [])       => (e :: l, None, [])
+    | (l, None, [])         => (l, None, [])
+    | (l, Some e, r :: rest) => (e :: l, Some r, rest)
+    | (l, None, r :: rest)   => (l, Some r, rest)
+    end.
+
+  Lemma mov_l_to_list (z : t) : to_list (mov_l z) = to_list z.
+  Proof.
+    destruct z as [[[|e_l l] [e|]] [|e_r r]];
+      simpl;
+      try rewrite <- app_assoc;
+      reflexivity.
+  Qed.
+
+  Lemma mov_r_to_list (z : t) : to_list (mov_r z) = to_list z.
+  Proof.
+    destruct z as [[[|e_l l] [e|]] [|e_r r]];
+      simpl;
+      try rewrite <- app_assoc;
+      reflexivity.
+  Qed.
+
+  Definition get (z : t) : option V :=
     match z with
     | (_, v, _) => v
     end.
@@ -38,47 +64,28 @@ Section defn.
     | (_, _, r) => r
     end.
 
-  Inductive left_of : t -> t -> Prop :=
-  | left_of0 : forall v v' l r,
-      left_of (l, v', (v :: r)) ((v' :: l), v, r)
-  | left_of1 : forall v v' l r z,
-      left_of z (l, v', (v :: r)) ->
-      left_of z ((v' :: l), v, r).
+  Definition del (z : t) : t :=
+    match z with
+    | (l, _, r) => (l, None, r)
+    end.
 
-  Definition left_eq (a b : t) : Prop :=
-    a = b \/ left_of a b.
+  Inductive left_eq : t -> t -> Prop :=
+  | left_eq0 : forall z,
+      left_eq z z
+  | left_eq1 : forall l m e r z',
+      left_eq (mov_r (l, m, e :: r)) z' ->
+      left_eq (l, m, e :: r) z'.
+
+  Definition left_of (a b : t) : Prop :=
+    left_eq (mov_r a) b.
 
   Section tests.
-    Goal forall l1 l2 m r, let z := ([l1; l2], m, r) in left_of (movl z) z.
+    Goal forall l1 l2 m r, let z := ([l1; l2], Some m, r) in left_eq (mov_l (mov_l z)) z.
     Proof. intros. simpl. repeat constructor. Qed.
 
-    Goal forall l1 l2 m r, let z := ([l1; l2], m, r) in left_eq (movl (movl z)) z.
-    Proof. intros. right. simpl. repeat constructor. Qed.
-
-    Goal forall l1 m r, let z := ([l1], m, r) in ~left_of z z.
-    Proof.
-      intros. intros H. subst z. inversion H. subst. inversion H2.
-    Qed.
-
-    Goal forall a b c z, left_of z ([a; b], c, []) -> get z = a \/ get z = b.
-    Proof.
-      intros. inversion H; subst.
-      - simpl. now left.
-      - inversion H2; subst; simpl.
-        + now right.
-        + inversion H3.
-    Qed.
+    Goal forall l1 l2 m r, let z := ([l1; l2], Some m, r) in left_of (mov_l z) z.
+    Proof. intros. simpl. repeat constructor. Qed.
   End tests.
-
-  Lemma left_of_length z z' :
-    left_of z z' ->
-    length (get_l z) < length (get_l z').
-  Proof.
-    intros H.
-    induction H; simpl.
-    - constructor.
-    - eapply PeanoNat.Nat.lt_trans; eauto.
-  Qed.
 
   Definition right_of (z1 z2 : t) : Prop :=
     left_of z2 z1.
@@ -88,52 +95,43 @@ Section defn.
 
   Definition Forall (P : V -> Prop) (z : t) : Prop :=
     match z with
-    | (l, v, r) => P v /\ List.Forall P l /\ List.Forall P r
+    | (l, None, r) => List.Forall P l /\ List.Forall P r
+    | (l, Some v, r) => P v /\ List.Forall P l /\ List.Forall P r
+    end.
+
+  Definition optional_filter (P : V -> bool) (v : option V) : option V :=
+    match v with
+    | Some v =>
+      match P v with
+      | true => Some v
+      | false => None
+      end
+    | None => None
     end.
 
   Definition filter (P : V -> bool) (z : t) : t :=
     match z with
-    | (l, v, r) => (List.filter P l, v, List.filter P r)
+    | (l, v, r) => (List.filter P l, optional_filter P v, List.filter P r)
+    end.
+
+  Definition of_list (l : list V) : t :=
+    match l with
+    | [] => ([], None, [])
+    | e :: rest => ([], Some e, rest)
     end.
 
   Definition zipper_of (z : t) (l : list V) : Prop :=
-    match l with
-    | a :: t =>
-      let z0 := ([], a, t) in
-      z = z0 \/ left_of z0 z
-    | [] => False
-    end.
-
-  Definition rewind (z : t) : t :=
-    match z with
-    | (l, e, r) =>
-      match rev l with
-      | [] => ([], e, r)
-      | e' :: l' => ([], e', l' ++ e :: r)
-      end
-    end.
-
-  Definition to_list (z : t) : list V :=
-    match z with
-    | (l, e, r) => rev l ++ (e :: r)
-    end.
+    left_eq (of_list l) z.
 
   Lemma left_of_to_list z1 z2 :
-    left_of z1 z2 ->
+    left_eq z1 z2 ->
     to_list z1 = to_list z2.
   Proof with auto.
     intros H.
     induction H.
-    - cbn. induction (rev l)...
-      cbn in *. rewrite IHl0...
-    - rewrite IHleft_of. cbn. rewrite <-app_assoc...
+    - reflexivity.
+    - now rewrite mov_r_to_list in IHleft_eq.
   Qed.
-
-  Definition of_list (l : list V) (default : V) : t :=
-    match l with
-    | [] => ([], default, [])
-    | e :: rest => ([], e, rest)
-    end.
 
   Lemma left_of_dec z1 z2 :
     to_list z1 = to_list z2 ->
@@ -150,24 +148,40 @@ Infix "<z" := (left_of)(at level 90) : zipper_scope.
 Infix "<=z" := (left_eq)(at level 90) : zipper_scope.
 
 Global Arguments t : clear implicits.
-Hint Constructors left_of : slot.
+Hint Constructors left_eq : slot.
 
 Section tests.
   Open Scope zipper_scope.
 
-  Let foo := ([2; 1], 3, [4]).
+  Let foo := ([2; 1], Some 3, [4]).
 
-  Goal ([], 1, [2; 3; 4]) <z foo.
+  Goal ([], Some 1, [2; 3; 4]) <z foo.
     repeat constructor.
   Qed.
 
-  Goal ([1], 2, [3; 4]) <=z foo.
+  Goal ([1], Some 2, [3; 4]) <=z foo.
     right. repeat constructor.
   Qed.
 
-  Goal not(([3; 2; 1], 4, []) <z foo).
+  Goal forall z, z <=z foo -> exists v, get z = Some v /\ v < 4.
+    intros z Hz.
+    subst foo.
+    inversion Hz; subst; cbn in *.
+    - exists 3. auto.
+    - destruct m.
+      + inversion H; subst.
+        * exists 2. auto.
+        * simpl in *. give_up.
+      +
+      inversion H; subst; cbn in *.
+      +
+
+
+  Goal not(([3; 2; 1], Some 4, []) <z foo).
     intros H. subst foo.
-    inversion H; subst. inversion H2; subst. inversion H3.
+    cbv in H.
+    inversion H; subst; cbv in *. inversion H0; subst. simpl
+    inversion H; subst. inversion H0; subst. inversion H1.
   Qed.
 
   Goal zipper_of ([], 1, [2; 3]) [1; 2; 3].
