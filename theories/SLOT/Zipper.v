c189a375077ac3854dafcd3b259803312bfd153e
k32 <10274441+k32@users.noreply.github.com>
2021-01-07 21:25:45 +0100

    Fix zipper with deletion

--- theories/SLOT/Zipper.v
+++ theories/SLOT/Zipper.v
@@ -33,6 +33,22 @@ Section defn.
     | (l, None, r :: rest)   => (l, Some r, rest)
     end.
 
+  Lemma mov_l_r l m r :
+    let z := (l, Some m, r) in
+    mov_r (mov_l z) = z.
+  Proof.
+    intros z. subst z.
+    now destruct l as [|e l].
+  Qed.
+
+  Lemma mov_r_l l m r :
+    let z := (l, Some m, r) in
+    mov_l (mov_r z) = z.
+  Proof.
+    intros z. subst z.
+    now destruct r as [|e r].
+  Qed.
+
   Lemma mov_l_to_list (z : t) : to_list (mov_l z) = to_list z.
   Proof.
     destruct z as [[[|e_l l] [e|]] [|e_r r]];
@@ -70,14 +86,14 @@ Section defn.
     end.
 
   Inductive left_eq : t -> t -> Prop :=
-  | left_eq0 : forall z,
-      left_eq z z
-  | left_eq1 : forall l m e r z',
-      left_eq (mov_r (l, m, e :: r)) z' ->
-      left_eq (l, m, e :: r) z'.
+  | left_eq0 : forall l m r,
+      left_eq (l, Some m, r) (l, Some m, r)
+  | left_eq1 : forall l m e r l' m' r',
+      left_eq (l, Some m, r) (l', Some m', e :: r') ->
+      left_eq (l, Some m, r) (m' :: l', Some e, r').
 
   Definition left_of (a b : t) : Prop :=
-    left_eq (mov_r a) b.
+    left_eq a (mov_l b).
 
   Section tests.
     Goal forall l1 l2 m r, let z := ([l1; l2], Some m, r) in left_eq (mov_l (mov_l z)) z.
@@ -87,11 +103,23 @@ Section defn.
     Proof. intros. simpl. repeat constructor. Qed.
   End tests.
 
-  Definition right_of (z1 z2 : t) : Prop :=
-    left_of z2 z1.
+  Inductive right_eq : t -> t -> Prop :=
+  | right_eq0 : forall l m r,
+      right_eq (l, Some m, r) (l, Some m, r)
+  | right_eq1 : forall l m e r l' m' r',
+      right_eq (l, Some m, r) (e :: l', Some m', r') ->
+      right_eq (l, Some m, r) (l', Some e, m' :: r').
 
-  Definition right_eq (z1 z2 : t) : Prop :=
-    left_eq z2 z1.
+  Lemma right_eq_left_eq_eqiv z1 z2 :
+    right_eq z1 z2 <-> left_eq z2 z1.
+  Proof with subst; simpl in *.
+    split; intros H.
+    - induction H.
+      + constructor.
+  Abort. (* TODO *)
+
+  Definition right_of (z1 z2 : t) : Prop :=
+    right_eq z1 (mov_r z2).
 
   Definition Forall (P : V -> Prop) (z : t) : Prop :=
     match z with
@@ -123,14 +151,14 @@ Section defn.
   Definition zipper_of (z : t) (l : list V) : Prop :=
     left_eq (of_list l) z.
 
-  Lemma left_of_to_list z1 z2 :
+  Lemma left_eq_to_list z1 z2 :
     left_eq z1 z2 ->
     to_list z1 = to_list z2.
   Proof with auto.
     intros H.
     induction H.
     - reflexivity.
-    - now rewrite mov_r_to_list in IHleft_eq.
+    - cbn in *. now rewrite <-app_assoc.
   Qed.
 
   Lemma left_of_dec z1 z2 :
@@ -146,6 +174,8 @@ Declare Scope zipper_scope.
 Delimit Scope zipper_scope with zipper.
 Infix "<z" := (left_of)(at level 90) : zipper_scope.
 Infix "<=z" := (left_eq)(at level 90) : zipper_scope.
+Infix ">z" := (right_of)(at level 90) : zipper_scope.
+Infix ">=z" := (right_eq)(at level 90) : zipper_scope.
 
 Global Arguments t : clear implicits.
 Hint Constructors left_eq : slot.
@@ -164,98 +194,39 @@ Section tests.
   Qed.
 
   Goal forall z, z <=z foo -> exists v, get z = Some v /\ v < 4.
-    intros z Hz.
-    subst foo.
-    inversion Hz; subst; cbn in *.
+  Proof with subst; cbn in *.
+    intros z Hz. subst foo.
+    inversion Hz...
     - exists 3. auto.
-    - destruct m.
-      + inversion H; subst.
-        * exists 2. auto.
-        * simpl in *. give_up.
-      +
-      inversion H; subst; cbn in *.
-      +
+    - inversion H1...
+      + exists 2. auto.
+      + inversion H0...
+        exists 1. auto.
+  Qed.
 
+  Goal forall z, z >z foo -> get z = Some 4.
+  Proof with subst; cbn in *.
+    intros z Hz. subst foo.
+    inversion Hz...
+    - reflexivity.
+  Qed.
 
   Goal not(([3; 2; 1], Some 4, []) <z foo).
     intros H. subst foo.
     cbv in H.
-    inversion H; subst; cbv in *. inversion H0; subst. simpl
-    inversion H; subst. inversion H0; subst. inversion H1.
+    inversion H; subst; cbv in *.
+    inversion H1.
   Qed.
 
-  Goal zipper_of ([], 1, [2; 3]) [1; 2; 3].
-    firstorder.
+  Goal zipper_of ([], Some 1, [2; 3]) [1; 2; 3].
+    repeat constructor.
   Qed.
 
-  Goal zipper_of ([1], 2, [3]) [1; 2; 3].
-    right. constructor.
+  Goal zipper_of ([1], Some 2, [3]) [1; 2; 3].
+    repeat constructor.
   Qed.
 
-  Goal zipper_of ([2; 1], 3, []) [1; 2; 3].
-    right. repeat constructor.
+  Goal zipper_of ([2; 1], Some 3, []) [1; 2; 3].
+    repeat constructor.
   Qed.
 End tests.
-
-Module OfLists.
-  (** * Zipper of nonempty lists *)
-  Section defns.
-    Context {V : Type}.
-
-    Definition t := t (list V).
-
-    Definition movl (z : t) : t :=
-      match z with
-      | ([], e, r) => ([], e, r)
-      | (l :: rest, [], r) => (rest, l, r)
-      | (l :: rest, e, r) => (rest, l, e :: r)
-      end.
-
-    Definition movr (z : t) : t :=
-      match z with
-      | (l, e, []) => (l, e, [])
-      | (l, [], r :: rest) => (l, r, rest)
-      | (l, e, r :: rest) => (e :: l, r, rest)
-      end.
-
-    Definition get (z : t) : list V :=
-      match z with
-      | (_, v, _) => v
-      end.
-
-    Let rewind_ (z : t) : t :=
-      (* equivalent to repeating movl many times: *)
-      let fix go l m r :=
-          match l, m with
-          | [],     _  => ([], m, r)
-          | e :: l', [] => go l' e r
-          | e :: l', _  => go l' e (m :: r)
-          end in
-      match z with
-      | (l, m, r) => go l m r
-      end.
-
-    Definition nonempty (l : list V) : bool :=
-      match l with
-      | [] => false
-      | _ :: _ => true
-      end.
-
-    Definition of_list (l : list (list V)) : t :=
-      match l with
-      | [] => ([], [], [])
-      | e :: rest => ([], e, List.filter nonempty rest)
-      end.
-
-    Definition rewind (z : t) : t :=
-      of_list (to_list z).
-
-    Goal forall a b l r, rewind ([a :: l], [], [b :: r]) = ([], a :: l, [b :: r]).
-      intros. reflexivity.
-    Qed.
-
-    Goal forall a b c l m r, rewind ([a :: l], c :: m, [b :: r]) = ([], a :: l, [c :: m; b :: r]).
-      intros. reflexivity.
-    Qed.
-  End defns.
-End OfLists.
