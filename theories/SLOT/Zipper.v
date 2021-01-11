43af4e8c67c1f78c3b471af6dd86ed5e72b8330c
k32 <10274441+k32@users.noreply.github.com>
2021-01-12 00:30:17 +0100

    Support None in left_eq and right_eq (part 1)

--- theories/SLOT/Zipper.v
+++ theories/SLOT/Zipper.v
@@ -89,11 +89,14 @@ Section defn.
     end.
 
   Inductive left_eq : t -> t -> Prop :=
-  | left_eq0 : forall z,
-      left_eq z z
-  | left_eq1 : forall z e l' m' r',
-      left_eq z (l', Some m', e :: r') ->
-      left_eq z (m' :: l', Some e, r').
+  | left_eq0 : forall l m r,
+      left_eq (l, Some m, r) (l, Some m, r)
+  | left_eq1 : forall l m r e l' m' r',
+      left_eq (l, Some m, r) (l', Some m', e :: r') ->
+      left_eq (l, Some m, r) (m' :: l', Some e, r')
+  | left_eq2 : forall l m r l' m' r',
+      left_eq (l, Some m, r) (l', Some m', r') ->
+      left_eq (l, Some m, r) (m' :: l', None, r').
 
   Definition left_of (a b : t) : Prop :=
     left_eq a (mov_l b).
@@ -107,11 +110,14 @@ Section defn.
   End tests.
 
   Inductive right_eq : t -> t -> Prop :=
-  | right_eq0 : forall z,
-      right_eq z z
-  | right_eq1 : forall z e l' m' r',
-      right_eq z (e :: l', Some m', r') ->
-      right_eq z (l', Some e, m' :: r').
+  | right_eq0 : forall l m r,
+      right_eq (l, Some m, r) (l, Some m, r)
+  | right_eq1 : forall l m r e l' m' r',
+      right_eq (l, Some m, r) (e :: l', Some m', r') ->
+      right_eq (l, Some m, r) (l', Some e, m' :: r')
+  | right_eq2 : forall l m r l' m' r',
+      right_eq (l, Some m, r) (l', Some m', r') ->
+      right_eq (l, Some m, r) (l', None, m' :: r').
 
   Lemma left_eq_mov_l e l m r l' m' r' :
     left_eq (e :: l, Some m, r) (l', Some m', r') ->
@@ -142,19 +148,35 @@ Section defn.
   Qed.
 
   Lemma right_eq_left_eq_equiv z1 z2 :
+    (* This property only holds when the zipper points at non-empty
+    element. This is easy to understand: our inductive definitions of
+    [right_eq] and [left_eq] are deliberately crafted in a way that
+    forbids appearance of zippers pointing at [None] on the left side
+    of the equation. Hence swapping positions of the arguments will
+    lead to terms that cannot be created. Hence we weaken this lemma
+    by the below premises, and it works: *)
+    (exists v1, get z1 = Some v1) ->
+    (exists v2, get z2 = Some v2) ->
     right_eq z1 z2 <-> left_eq z2 z1.
   Proof with subst; simpl in *.
+    intros H1 H2.
     split; intros H.
     - induction H.
       + constructor.
-      + inversion IHright_eq; subst.
+      + apply IHright_eq in H1.
+        2:{ exists m'. reflexivity. }
+        inversion H1; subst.
         * constructor. constructor.
         * apply left_eq_mov_l. now constructor.
+      + exfalso. now destruct H2.
     - induction H.
       + constructor.
-      + inversion IHleft_eq; subst.
+      + apply IHleft_eq in H2.
+        2:{ exists m'. reflexivity. }
+        inversion H2; subst.
         * constructor. constructor.
-        * constructor. now apply right_eq_mov_r.
+        * apply right_eq_mov_r. now constructor.
+      + exfalso. now destruct H1.
   Qed.
 
   Definition right_of (z1 z2 : t) : Prop :=
@@ -197,6 +219,7 @@ Section defn.
     intros H.
     induction H.
     - reflexivity.
+    - cbn in *. now rewrite <-app_assoc.
     - cbn in *. now rewrite <-app_assoc.
   Qed.
 
@@ -242,6 +265,21 @@ Section tests.
     - auto.
     - inversion H1...
       + auto.
+      + inversion H0...
+        auto.
+  Qed.
+
+  Goal forall z, z <=z ([3; 2; 1], None, [4]) ->
+            get z = Some 3 \/
+            get z = Some 2 \/
+            get z = Some 1.
+  Proof with subst; cbn in *.
+    intros z Hz. subst foo.
+    inversion Hz...
+    inversion H1...
+    - auto.
+    - inversion H0...
+      + auto.
       + inversion H2...
         auto.
   Qed.
@@ -257,7 +295,7 @@ Section tests.
     intros H. subst foo.
     cbv in H.
     inversion H; subst; cbv in *.
-    inversion H2.
+    inversion H1.
   Qed.
 
   Goal zipper_of ([], Some 1, [2; 3]) [1; 2; 3].
