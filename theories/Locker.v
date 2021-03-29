87c3299961b0ed4ad48e2e2748e96ecd2f60e905
k32 <10274441+k32@users.noreply.github.com>
2021-03-29 22:35:44 +0200

    Add GPL copyright header to the source code files

--- theories/Locker.v
+++ theories/Locker.v
@@ -1,3 +1,18 @@
+(* LibTx, proofs about distributed systems design
+   Copyright (C) 2019-2021  k32
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, version 3 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*)
 From Coq Require Import
      Lists.List
      Arith.Le
